aed98977678407ea3af1a11c8fcf5e0dca148ffb
seba <seba@minero.casa>
2018-01-08 02:01:12 -0300

    build and configs

---
+++ build/CMakeFiles/xmrig-amd.dir/DependInfo.cmake
@@ -0,0 +1,93 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/seba/repos/xmrig-amd/src/crypto/c_blake256.c" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/crypto/c_blake256.c.o"
+  "/home/seba/repos/xmrig-amd/src/crypto/c_groestl.c" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/crypto/c_groestl.c.o"
+  "/home/seba/repos/xmrig-amd/src/crypto/c_jh.c" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/crypto/c_jh.c.o"
+  "/home/seba/repos/xmrig-amd/src/crypto/c_keccak.c" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/crypto/c_keccak.c.o"
+  "/home/seba/repos/xmrig-amd/src/crypto/c_skein.c" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/crypto/c_skein.c.o"
+  "/home/seba/repos/xmrig-amd/src/crypto/soft_aes.c" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/crypto/soft_aes.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "HAVE_SYSLOG_H"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "XMRIG_AMD_PROJECT"
+  "XMRIG_NO_LIBCPUID"
+  "_GNU_SOURCE"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/3rdparty"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/seba/repos/xmrig-amd/src/App.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/App.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/App_unix.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/App_unix.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/Console.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/Console.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/Cpu_stub.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/Cpu_stub.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/Cpu_unix.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/Cpu_unix.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/Options.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/Options.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/Platform.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/Platform.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/Platform_unix.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/Platform_unix.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/Summary.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/Summary.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/amd/OclCLI.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/amd/OclCLI.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/amd/OclGPU.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/amd/OclGPU.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/api/Api.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/api/Api.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/api/ApiState.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/api/ApiState.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/api/Httpd.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/api/Httpd.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/api/NetworkState.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/api/NetworkState.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/crypto/CryptoNight.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/crypto/CryptoNight.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/log/ConsoleLog.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/log/ConsoleLog.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/log/FileLog.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/log/FileLog.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/log/Log.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/log/Log.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/log/SysLog.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/log/SysLog.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/net/Client.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/net/Client.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/net/Job.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/net/Job.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/net/Network.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/net/Network.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/net/SubmitResult.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/net/SubmitResult.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/net/Url.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/net/Url.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/net/strategies/DonateStrategy.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/net/strategies/DonateStrategy.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/net/strategies/FailoverStrategy.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/net/strategies/FailoverStrategy.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/net/strategies/SinglePoolStrategy.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/net/strategies/SinglePoolStrategy.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/workers/Handle.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/workers/Handle.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/workers/Hashrate.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/workers/Hashrate.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/workers/OclThread.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/workers/OclThread.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/workers/OclWorker.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/workers/OclWorker.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/workers/Workers.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/workers/Workers.cpp.o"
+  "/home/seba/repos/xmrig-amd/src/xmrig.cpp" "/home/seba/repos/xmrig-amd/build/CMakeFiles/xmrig-amd.dir/src/xmrig.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_SYSLOG_H"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "XMRIG_AMD_PROJECT"
+  "XMRIG_NO_LIBCPUID"
+  "_GNU_SOURCE"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/3rdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
